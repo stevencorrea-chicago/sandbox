adbc6993566de767e5fbb5390c82dfff5447a369
stevencorrea-chicago <40609698+stevencorrea-chicago@users.noreply.github.com>
2023-12-25 16:11:23 -0600

    testing

--- Data_Science/test.py
+++ Data_Science/test.py
@@ -1,6 +1,20 @@
 import pandas as pd
 
+columns = ['Host Id', 'Host Since', 'Name', 'Neighbourhood', 'Property Type',
+       'Review Scores Rating (bin)', 'Room Type', 'Zipcode', 'Beds',
+       'Number of Records', 'Number Of Reviews', 'Price',
+       'Review Scores Rating']
+
 df = pd.read_excel(r'C:\Users\steve\Downloads\airbnb.xlsx')
 df = df[~ df['Host Since'].isna()]
+df.columns = columns
 
-print(df['Property Type'].value_counts())
+print(df.columns)
+
+#print(df['Property Type'].value_counts())
+
+#print(df['Host Since'].sort_values().head(1))
+
+#print(df['Host Since'].sort_values().tail(1))
+
+print(df[df['Neighbourhood'] == 'Queens']['Zipcode'].value_counts())
