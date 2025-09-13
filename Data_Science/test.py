8d4e98dada86cccc01d3bf0601afdee171e11969
stevencorrea-chicago <40609698+stevencorrea-chicago@users.noreply.github.com>
2025-09-13 14:57:18 -0500

    Code change

--- Data_Science/test.py
+++ Data_Science/test.py
@@ -1,22 +1,21 @@
 import pandas as pd
+from pathlib import Path
 
 columns = ['Host Id', 'Host Since', 'Name', 'Neighbourhood', 'Property Type',
        'Review Scores Rating (bin)', 'Room Type', 'Zipcode', 'Beds',
        'Number of Records', 'Number Of Reviews', 'Price',
        'Review Scores Rating']
 
-df = pd.read_excel(r'C:\Users\steve\Downloads\airbnb.xlsx')
-df = df[~ df['Host Since'].isna()]
-df.columns = columns
+script_path = Path(__file__).resolve()
+parent_dir = script_path.parent
+
+print(script_path)
+print(parent_dir)
+
+# Recommended way: use `/` to join paths
+file_path = parent_dir.parent/ "mock_airbnb_data_500.xlsx"
+
+df = pd.read_excel(file_path)
 
 print(df.columns)
-
-#print(df['Property Type'].value_counts())
-
-#print(df['Host Since'].sort_values().head(1))
-
-#print(df['Host Since'].sort_values().tail(1))
-
-mask = df['Neighbourhood'] == 'Queens'
-df[mask]['Zipcode'].to_csv('output.csv', index=False)
 
